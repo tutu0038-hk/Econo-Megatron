8ab2a82270d17ab212a87fb045ea3861c3f4ec3e
王嘉树 <wangjiashu@wangjiashudeMacBook-Pro.local>
2024-12-05 20:42:03 +0800

    upgrade llama model

--- megatron/core/models/LLaMA/llama_model.py
+++ megatron/core/models/LLaMA/llama_model.py
@@ -71,9 +71,6 @@ class LLaMAModel(MegatronModule):
             pre_process=self.pre_process,
             post_process=self.post_process)
 
-        if not args.untie_embeddings_and_output_weights:
-            self.initialize_word_embeddings()
-        
         if self.post_process:
             self.lm_head = torch.nn.Linear(args.hidden_size, args.padded_vocab_size, bias=False)
 
